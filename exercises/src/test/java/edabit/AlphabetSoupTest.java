1ca61b98e0b24c5ec7c5e5a765c5bbb4fc42a973
anegowska <a.negowska@wp.pl>
2019-02-20 22:07:42 +0100

    test for AlphabetSoup exercise

---
+++ exercises/src/test/java/edabit/AlphabetSoupTest.java
@@ -0,0 +1,23 @@
+package edabit;
+
+import org.junit.Test;
+
+import static edabit.AlphabetSoup.alphabetSoup;
+import static org.junit.Assert.*;
+
+public class AlphabetSoupTest {
+
+    @Test
+    public void shouldReturnStringInAlphabeticalOrder() {
+        // given
+        String s = "JavaScript";
+
+        String expectedString = "aacijprstv";
+
+        // when
+        String result = alphabetSoup(s);
+
+        // then
+        assertEquals(result, expectedString);
+    }
+}
